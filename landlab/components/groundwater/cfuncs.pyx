29a1f4e9825ab321b1c9d598fbb591ce8ffe8277
David Litwin <davidglitwin@posteo.net>
2021-06-11 21:06:53 -0400

    working functions

--- landlab/components/groundwater/cfuncs.pyx
+++ landlab/components/groundwater/cfuncs.pyx
@@ -9,38 +9,62 @@ ctypedef np.int_t DTYPE_INT_t
 DTYPE_FLOAT = np.double
 ctypedef np.double_t DTYPE_FLOAT_t
 
-# @cython.boundscheck(False)
-# @cython.wraparound(False)
-def _calc_grad_at_link(np.ndarray[DTYPE_FLOAT_t, ndim=1] values,
-          np.ndarray[DTYPE_INT_t, ndim=1] node_head,
-          np.ndarray[DTYPE_INT_t, ndim=1] node_tail,
-          np.ndarray[DTYPE_FLOAT_t, ndim=1] link_lengths,
-          ):
+# # regularization functions used to deal with numerical demons of seepage
+# cdef _regularize_G(np.ndarray[DTYPE_FLOAT_t, ndim=1] u, DTYPE_FLOAT_t reg_factor):
+#     """Smooths transition of step function with an exponential. 0<=u<=1."""
+#     return np.exp(-(1 - u) / reg_factor)
+#
+#
+# cdef _regularize_R(np.ndarray[DTYPE_FLOAT_t, ndim=1] u):
+#     """ramp function on u."""
+#     return u * np.greater_equal(u, 0.)
+#
+#
+# def fun_dhdt(float t,
+#             np.ndarray[DTYPE_FLOAT_t, ndim=1] h,
+#             np.ndarray[DTYPE_FLOAT_t, ndim=1] f,
+#             np.ndarray[DTYPE_FLOAT_t, ndim=1] dqdx,
+#             np.ndarray[DTYPE_FLOAT_t, ndim=1] reg_thickness,
+#             np.ndarray[DTYPE_FLOAT_t, ndim=1] n,
+#             float reg_factor):
+#     """generate a function to pass to solve_ivp"""
+#     cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] out = (
+#         1
+#         / n
+#         * (
+#             f
+#             - dqdx
+#             - _regularize_G(h / reg_thickness, reg_factor) * _regularize_R(f - dqdx)
+#         )
+#     return out
+#
+# def _calc_grad_at_link(np.ndarray[DTYPE_FLOAT_t, ndim=1] values,
+#           np.ndarray[DTYPE_INT_t, ndim=1] node_head,
+#           np.ndarray[DTYPE_INT_t, ndim=1] node_tail,
+#           np.ndarray[DTYPE_FLOAT_t, ndim=1] link_lengths,
+#           ):
+#
+# 	cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] out = np.empty(len(node_head), dtype=float)
+# 	return np.divide(
+# 		values[node_head] - values[node_tail],
+# 		link_lengths,
+# 		out=out,
+# 	)
 
-	cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] out = np.empty(len(node_head), dtype=float)
-	return np.divide(
-		values[node_head] - values[node_tail],
-		link_lengths,
-		out=out,
-	)
 
-
-# @cython.boundscheck(False)
-# @cython.wraparound(False)
 def _calc_flux_div_at_node(np.ndarray[DTYPE_FLOAT_t, ndim=1] unit_flux,
           np.ndarray[DTYPE_INT_t, ndim=1] node_at_cell,
           np.ndarray[DTYPE_INT_t, ndim=1] link_at_face,
           np.ndarray[DTYPE_INT_t, ndim=2] faces_at_cell,
           np.ndarray[DTYPE_FLOAT_t, ndim=1] area_of_cell,
           np.ndarray[DTYPE_FLOAT_t, ndim=1] length_of_face,
-          np.ndarray[DTYPE_INT_t, ndim=2] link_dirs_at_node,
-          int number_of_cells,
-          int number_of_nodes,
+          np.ndarray[signed char, ndim=2] link_dirs_at_node,
           ):
 
   cdef int number_of_dirs = link_dirs_at_node.shape[1]
+  cdef int number_of_nodes = link_dirs_at_node.shape[0]
   cdef int c
-  cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] buf = np.empty(number_of_cells, dtype=float)
+  cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] buf = np.empty_like(area_of_cell)
   cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] out = np.empty(number_of_nodes, dtype=float)
   cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] total_flux = unit_flux[link_at_face] * length_of_face
 
